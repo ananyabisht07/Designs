c4e77cf442dd62489fffadb24d5cac57fbd5f3f1
ananyabisht07 <ananya.bisht@yahoo.in>
2020-07-23 20:22:53 +0530

    Animated Background

---
+++ layouts/src/Components/Bg.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Bg(){
+    return(
+        <div className="animated-area">
+            <ul className="box-area">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Bg;
