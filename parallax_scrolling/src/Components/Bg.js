38c64e3e4872b1f1e317737a7d77f0d81ee84a76
ananyabisht07 <ananya.bisht@yahoo.in>
2020-07-24 21:49:53 +0530

    added javascript

--- parallax_scrolling/src/Components/Bg.js
+++ parallax_scrolling/src/Components/Bg.js
@@ -1,13 +1,17 @@
 import React,{Component} from 'react'
 
 class Bg extends Component{
+    componentDidMount(){
+        window.test();
+    }
     render(){
         return(
             <section className="bg-style">
-                <img src="img/bg.jpg" alt="bg" />
-                <img src="img/moon.png" alt="moon" />
-                <img src="img/mountain.png" alt="mountain" />
-                <img src="img/road.png" alt="road" />
+                <img src="img/bg.jpg" id="bg"  alt="bg" />
+                <img src="img/moon.png" id="moon" alt="moon" />
+                <img src="img/mountain.png" id="mountain" alt="mountain" />
+                <img src="img/road.png" id="road" alt="road" />
+                <h2 id="text">Moon Light</h2>
             </section>
         )
     }
